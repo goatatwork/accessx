fb7f5b638e6aeef2f6eb076d1e7f48361ae041e2
The Goat <ryantgray@gmail.com>
2018-03-06 11:04:53 -0600

    Bootstrap 4 service location card and provisioning pages

--- resources/assets/js/components/GoldAccess/Provisioning/ProvisionByServiceLocation.vue
+++ resources/assets/js/components/GoldAccess/Provisioning/ProvisionByServiceLocation.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="row">
-        <div class="col-md-12">
+    <div class="container-fluid">
 
-            <div class="row">
-                <div class="col-md-4">
-                    <service-location-card :location="location"></service-location-card>
-                </div>
-                <div class="col-md-8">
-                    <provision-by-service-location-form :location="location"></provision-by-service-location-form>
-                </div>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="/provisioning">Provisioning</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Provisioning By Service Location</li>
+            </ol>
+        </nav>
+
+        <div class="row">
+            <div class="col-4">
+                <service-location-card :location="location"></service-location-card>
             </div>
-
+            <div class="col-8">
+                <provision-by-service-location-form :location="location"></provision-by-service-location-form>
+            </div>
         </div>
+
     </div>
 </template>
 

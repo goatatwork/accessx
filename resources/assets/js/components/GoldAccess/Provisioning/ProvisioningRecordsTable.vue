2b33271d8769098b13e5ae6c440b3d686feae3fd
The Goat <ryantgray@gmail.com>
2018-03-06 12:13:16 -0600

    Create component

---
+++ resources/assets/js/components/GoldAccess/Provisioning/ProvisioningRecordsTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <pre>{{ provisioningRecords }}</pre>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            provisioningRecords: {}
+        }
+    }
+</script>
--- resources/views/provisioning/index.blade.php
+++ resources/views/provisioning/index.blade.php
@@ -13,10 +13,8 @@
 
     <div class="row">
         <div class="col">
-
             <div class="card">
                 <div class="card-body">
-
                     <div class="row">
                         <div class="col">
                             <dl>
@@ -28,12 +26,51 @@
                             <a href="/provisioning/create" class="btn btn-secondary float-right">Provisioning A Service</a>
                         </div>
                     </div>
-
                 </div>
             </div>
-
         </div>
     </div>
+
+    <provisioning-records-table :provisioning-records="{{ $provisioning_records }}"></provisioning-records-table>
+
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Customer</th>
+                        <th>Location</th>
+                        <th>Package</th>
+                        <th>Management IP</th>
+                        <th>ONT</th>
+                        <th>NetLocation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($provisioning_records as $record)
+                        <repeating-table-row>
+                            <tr slot="table-row">
+                                <td>{{ $record->service_location->customer->customer_name }}</td>
+                                <td>{{ $record->service_location->address1 }}, {{ $record->service_location->zip }}</td>
+                                <td>{{ $record->ont_profile->name }}</td>
+                                <td>{{ $record->ip_address->address }}</td>
+                                <td>{{ $record->ont_profile->ont_software->ont->model_number }}</td>
+                                <td>
+                                    {{ $record->port->slot->aggregator->name }}
+                                    <span class="fas fa-long-arrow-alt-right"></span>
+                                    Slot {{ $record->port->slot->slot_number }}
+                                    <span class="fas fa-long-arrow-alt-right"></span>
+                                    Port {{ $record->port->port_number }}
+                                </td>
+                            </tr>
+                        </repeating-table-row>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
 </div>
 
 @endsection

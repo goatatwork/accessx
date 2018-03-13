5f1b0e9b814a6665d9cfdda35116b8b811e546de
The Goat <ryantgray@gmail.com>
2018-03-13 16:30:48 -0500

    Fix ActivityLogTest

--- tests/Feature/ActivityLogTest.php
+++ tests/Feature/ActivityLogTest.php
@@ -19,8 +19,8 @@ class ActivityLogTest extends TestCase
         app('logbot')->log('I am a test log', 'crit');
 
         $this->assertDatabaseHas('activity_logs', [
-            'calling_class' => 'Tests\\Feature\\ActivityLogTest',
-            'calling_function' => 'test_activity_log',
+            'calling_class' => 'App\\GoldAccess\\Utilities\\Logger',
+            'calling_function' => 'log',
             'level' => 'crit',
             'message' => 'I am a test log'
         ]);

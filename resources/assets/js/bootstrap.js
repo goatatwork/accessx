9fec2c9212506e1a58c46eb3929d25460a29a93d
The Goat <ryantgray@gmail.com>
2018-03-23 10:06:55 -0500

    Some tweaks

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -56,7 +56,8 @@ import Echo from 'laravel-echo'
 if (typeof io !== 'undefined') {
   window.Echo = new Echo({
       broadcaster: 'socket.io',
-      host: window.location.hostname + ':6001'
+      // host: window.location.hostname + ':6001'
+      host: 'http://localhost:6001'
   });
 } else {
     console.log('typeof io is undefined, wtf!?');

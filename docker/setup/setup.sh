095c3298bddee17136bdd58744e455aec08727b6
The Goat <ryantgray@gmail.com>
2018-03-23 20:33:25 -0500

    setup script

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # 20180323 Goat
 # This is intended to be run from the docker HOST. Not within the container
-docker exec -it -u www-data accessx_php_1 composer install --no-dev --ignore-platform-reqs
-docker exec -it -u www-data accessx_php_1 php artisan key:generate
-docker exec -it -u www-data accessx_php_1 php artisan migrate
-docker exec -it -u www-data accessx_php_1 php artisan db:seed
-docker exec -it -u www-data accessx_php_1 php artisan passport:install
+
+
+if [ $# -gt 2 ]; then
+    docker exec -it -u www-data accessx_php_1 sed -i 's/^APP_URL=http:\/\/accessx\.goat/APP_URL=http:\/\/10\.200\.200\.1/g' .env
+    docker exec -it -u www-data accessx_php_1 sed -i 's/\"authHost\": \"http:\/\/accessx\.goat\"/\"authHost\": \"http:\/\/10\.200\.200\.1\"/g' laravel-echo-server.json
+    docker exec -it -u www-data accessx_php_1 sed -i 's/^DB_PASSWORD=/DB_PASSWORD=1q2w3e4r/g' .env
+    docker exec -it -u www-data accessx_php_1 composer install --no-dev --ignore-platform-reqs
+    docker exec -it -u www-data accessx_php_1 php artisan key:generate
+    docker exec -it -u www-data accessx_php_1 php artisan migrate
+    docker exec -it -u www-data accessx_php_1 php artisan db:seed
+    docker exec -it -u www-data accessx_php_1 php artisan passport:install
+else
+    echo "Error: missing arguments"
+    echo "Usage: \"bash docker/setup/setup.sh <hostname-or-ip> <db-password>\""
+fi

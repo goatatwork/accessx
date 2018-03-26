d38b587287ddd0564018228cebf422b1b780320e
The Goat <ryantgray@gmail.com>
2018-03-26 10:01:42 -0500

    update

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -2,9 +2,6 @@
 # 20180323 Goat
 # This is intended to be run from the docker HOST. Not within the container
 
-sed -i 's/^APP_URL=http:\/\/accessx\.goat/APP_URL=http:\/\/10\.200\.200\.1/g' .env
-sed -i 's/\"authHost\": \"http:\/\/accessx\.goat\"/\"authHost\": \"http:\/\/10\.200\.200\.1\"/g' laravel-echo-server.json
-sed -i 's/^DB_PASSWORD=/DB_PASSWORD=1q2w3e4r/g' .env
 docker exec -it -u www-data accessx_php_1 composer install --no-dev --ignore-platform-reqs
 docker exec -it -u www-data accessx_php_1 php artisan key:generate
 docker exec -it -u www-data accessx_php_1 php artisan migrate

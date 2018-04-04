bd469a8b73a10b88e593425222db1291743dd929
The Goat <ryantgray@gmail.com>
2018-04-04 16:09:37 -0500

    update

--- docker/setup/setup.sh
+++ docker/setup/setup.sh
@@ -8,6 +8,7 @@ docker exec -it -u www-data accessx_php_1 php artisan migrate
 docker exec -it -u www-data accessx_php_1 php artisan db:seed
 docker exec -it -u www-data accessx_php_1 php artisan passport:install
 docker exec -it -u www-data accessx_php_1 php artisan storage:link
+docker exec -it -u root accessx_php_1 chown -R www-data.www-data .
 docker exec -it -u root accessx_php_1 chown -R www-data.www-data *
 
 # docker run -d --net=host -v $(pwd)/storage/app/services/dnsmasq:/etc/dnsmasq -v $(pwd)/public/storage/media:/tftpboot -p 67:67 -p 67:67/udp -p 69:69 -p 69:69/udp --rm --name dnsmasq_server goatatwork/dnsmasq-ubuntu:latest

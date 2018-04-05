8da36d18deefc36adc8a4d4aef9f553421fef894
The Goat <ryantgray@gmail.com>
2018-04-05 16:42:49 -0500

    change broadcast announcement

--- app/Events/SubnetWasCreated.php
+++ app/Events/SubnetWasCreated.php
@@ -45,8 +45,8 @@ class SubnetWasCreated implements ShouldBroadcast
      */
     public function broadcastWith()
     {
-        $user = Auth::user()->name ?: 'some user';
         $subnet = $this->subnet->network_address ?: 'some subnet';
-        return ['message' => $user . ' added subnet ' . $subnet . '.'];
+
+        return ['message' => 'Subnet ' . $subnet . ' was added.'];
     }
 }

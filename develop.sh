9d98350bd7d3f69f0369d23cdbf2e44110179118
The Goat <ryantgray@gmail.com>
2018-04-05 10:30:47 -0500

    develop script created

---
+++ develop.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Set environment variables
+export WEB_PORT=${WEB_PORT:-80}
+export DB_PORT=${DB_PORT:-3306}
+
+# if the number of arguments passed to this script is gt (greater than) zero
+# then pass those arguments onto the docker-compose command. If no
+# arguments are passed, just run docker-compose ps
+if [ $# -gt 0];then
+    docker-compose "$@"
+else
+    docker-compose ps
+fi

a4c9ef39e929afaa5dfb2b6bc57ffcf9370cf73d
The Goat <ryantgray@gmail.com>
2018-04-05 10:47:42 -0500

    fix typo

--- develop.sh
+++ develop.sh
@@ -7,7 +7,7 @@ export DB_PORT=${DB_PORT:-3306}
 # if the number of arguments passed to this script is gt (greater than) zero
 # then pass those arguments onto the docker-compose command. If no
 # arguments are passed, just run docker-compose ps
-if [ $# -gt 0];then
+if [ $# -gt 0 ];then
     docker-compose "$@"
 else
     docker-compose ps

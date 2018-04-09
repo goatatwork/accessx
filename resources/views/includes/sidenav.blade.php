42c88e580d501a6f15ffcd131b69d72f54ea26b2
The Goat <ryantgray@gmail.com>
2018-04-09 14:34:04 -0500

    Create side navbar

---
+++ resources/views/includes/sidenav.blade.php
@@ -0,0 +1,59 @@
+<aside class="col col-auto p-0 text-right bg-dark"> <!-- Sidenav -->
+    <nav class="navbar navbar-expand navbar-dark bg-dark flex-md-column flex-row align-items-start">
+        <div class="collapse navbar-collapse">
+            <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+
+                <li class="nav-item text-left text-warning">
+                    <a class="nav-link pl-0 text-warning" href="/">
+                        <span class="fas fa-tachometer-alt"></span> DASHBOARD
+                    </a>
+                </li>
+
+                <li class="nav-item text-center text-warning">
+                    <hr class="bg-dark">
+                </li>
+
+                <li class="nav-item text-left text-warning">
+                    <span class="fas fa-users"></span> CUSTOMERS
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/provisioning">Provisioned</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/customers">All Customers</a>
+                </li>
+
+                <li class="nav-item text-center text-warning">
+                    <hr class="bg-dark">
+                </li>
+
+                <li class="nav-item text-left text-warning">
+                    <span class="fas fa-sitemap"></span> NETWORK
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/dhcp">DHCP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/onts">ONTs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/infrastructure/aggregators">Aggregators</a>
+                </li>
+
+                <li class="nav-item text-center text-warning">
+                    <hr class="bg-dark">
+                </li>
+
+                <li class="nav-item text-left text-warning">
+                    <span class="fas fa-cogs"></span> SYSTEM
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="#">Users</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link pl-0 text-white" href="/activity_logs">Activity Logs</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</aside> <!-- /Sidenav -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,57 +28,65 @@
 
 </head>
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            @if (Auth::check())
-                <presence-information></presence-information>
-            @endif
+                @if (Auth::check())
+                    <presence-information></presence-information>
+                @endif
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    @if (Auth::guest())
-                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                <a href="{{ route('logout') }}" class="dropdown-item"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        @if (Auth::guest())
+                            <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">
+                                    {{ Auth::user()->name }}
                                 </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                    <a href="{{ route('logout') }}" class="dropdown-item"
+                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </div>
-                        </li>
-                    @endif
-                </ul>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </div>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+
             </div>
+        </nav>
 
+        <div class="container-fluid h-100">
+
+            <div class="row h-100">
+
+                @include('includes.sidenav')
+
+                <main class="col">
+                    @yield('content')
+                </main>
+
+            </div>
         </div>
-    </nav>
 
-    @yield('content')
-
-    @if(Auth::check())
-        <echo-messages></echo-messages>
-    @endif
-</div>
+    </div>
 
 <!-- Scripts -->
 <script src="//{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>

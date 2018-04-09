7110824308ba1afeaf16a168f63a2fc6ed0e376f
The Goat <ryantgray@gmail.com>
2018-04-09 14:51:33 -0500

    Add icon to brand logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <span class="fas fa-at"></span> {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

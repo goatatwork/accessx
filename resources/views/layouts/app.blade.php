1d16bc32d66e0e15c097cb6b3436f8504bdc6823
The Goat <ryantgray@gmail.com>
2018-04-11 12:14:23 -0500

    Fix quotes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <!-- Font Awesome -->
-    <script src=“https://use.fortawesome.com/a97ca672.js”></script>
+    <script src="https://use.fortawesome.com/a97ca672.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
     @if (Auth::check())

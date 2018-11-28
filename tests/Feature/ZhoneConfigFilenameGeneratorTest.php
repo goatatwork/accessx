07a93327b760f2c2c209f2fcbbaf1eb977d00739
Goat <ryantgray@gmail.com>
2018-11-28 15:17:07 -0600

    Refactor some tests, mostly just names

--- tests/Feature/ZhoneConfigFilenameGeneratorTest.php
+++ tests/Feature/ZhoneConfigFilenameGeneratorTest.php
@@ -12,10 +12,20 @@ use App\GoldAccess\Ont\ZhoneConfigFilenameGenerator;
 class ZhoneConfigFilenameGeneratorTest extends TestCase
 {
     /**
+     * @test
+     */
+    public function test_the_generator_requires_the_ontsoftware_to_work_with()
+    {
+        $this->expectException('TypeError');
+
+        $config_filename_generator = new ZhoneConfigFilenameGenerator();
+    }
+
+    /**
      *
      * @return void
      */
-    public function test_generator_can_generate_config_filename()
+    public function test_the_generator_can_generate_config_filename_that_the_ont_will_ask_for_based_on_the_ontsoftware()
     {
         $ont = factory(Ont::class)->create();
         $ont_software = factory(OntSoftware::class)->create(['ont_id' => $ont->id, 'version' => 'S03.01.266']);
@@ -29,7 +39,7 @@ class ZhoneConfigFilenameGeneratorTest extends TestCase
      *
      * @return void
      */
-    public function test_generator_can_generate_config_filename_for_27xx_models()
+    public function test_the_generator_can_generate_config_filename_for_27xx_models_that_the_ont_will_ask_for_based_on_the_ontsoftware()
     {
         $ont = factory(Ont::class)->create(['model_number' => '2728A1']);
         $ont_software = factory(OntSoftware::class)->create(['ont_id' => $ont->id, 'version' => 'S04.01.086']);

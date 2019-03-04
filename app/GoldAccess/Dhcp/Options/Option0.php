bc6cb502279aa9458c86024f35dba1718683015c
Goat <ryantgray@gmail.com>
2019-03-04 13:22:13 -0600

    Fix typo in Option0 configuration

--- app/GoldAccess/Dhcp/Options/Option0.php
+++ app/GoldAccess/Dhcp/Options/Option0.php
@@ -23,7 +23,7 @@ class Option0 extends DhcpOption
             'dhcp-range=set:"'.$dnsmasq_tag.'",' . (new self)->rangeFor($deployable) . ',1h' . "\n" .
             'tag-if=set:internet-pool,tag:' . $dnsmasq_tag . "\n" .
             'dhcp-option=tag:"'.$dnsmasq_tag.'",3,'.$deployable->routers. "\n" .
-            'dhcp-option=tag:"' . $dnsmasq_tag . '",1,' . $deployable->subnet_netmask . "\n" .
+            'dhcp-option=tag:"' . $dnsmasq_tag . '",1,' . $deployable->subnet_mask . "\n" .
             'dhcp-option=tag:"' . $dnsmasq_tag . '",5,' . $deployable->dns_servers . "\n" .
             'dhcp-option=tag:"' . $dnsmasq_tag . '",6,' . $deployable->dns_servers . "\n";
     }

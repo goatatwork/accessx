2f6952d73e3e236574fe3b358563ef152002fd7d
Goat <ryantgray@gmail.com>
2019-03-03 23:47:12 -0600

    Complete rewrite of Dhcpbot

--- app/GoldAccess/Dhcp/Dhcpbot.php
+++ app/GoldAccess/Dhcp/Dhcpbot.php
@@ -5,7 +5,10 @@ namespace App\GoldAccess\Dhcp;
 use Storage;
 use App\Subnet;
 use App\DhcpSharedNetwork;
+use Spatie\MediaLibrary\Models\Media;
+use App\GoldAccess\Dhcp\Options\Option0;
 use App\GoldAccess\Dhcp\Options\Option43;
+use App\GoldAccess\Utilities\MediaLibraryPathGenerator;
 
 class Dhcpbot
 {
@@ -15,16 +18,22 @@ class Dhcpbot
     public $bio = 'I am Dhcpbot';
 
     /**
-     * The Medialibrary media collection to store these in
+     * Name of the disk configured in filesystems.php
      * @var string
      */
-    public $media_collection = 'dhcp_files';
+    public $dhcp_origins_disk;
 
     /**
      * Name of the disk configured in filesystems.php
      * @var string
      */
-    public $media_disk;
+    public $dhcp_configs_disk;
+
+    /**
+     * Used to generate media paths
+     * @var \App\GoldAccess\Utilities\MediaLibraryPathGenerator
+     */
+    public $medialibraray_path_generator;
 
     /**
      * dhcp_temp disk for writing files before adding them to media library
@@ -35,177 +44,163 @@ class Dhcpbot
     public function __construct()
     {
         $this->temp_storage = Storage::disk('dhcp_temp');
-        $this->media_disk = (env('APP_ENV') == 'testing') ? 'dhcp_configs_test' : 'dhcp_configs';
+        $this->medialibraray_path_generator = new MediaLibraryPathGenerator();
+        $this->dhcp_origins_disk = (env('APP_ENV') == 'testing') ? 'dhcp_origins_testing' : 'dhcp_origins';
+        $this->dhcp_configs_disk = (env('APP_ENV') == 'testing') ? 'dhcp_configs_testing' : 'dhcp_configs';
     }
 
     /**
+     * Get current DHCP state for a subnet
      * @param  Subnet $subnet
+     * @return collection
+     */
+    public function report(Subnet $subnet)
+    {
+        $subnet->refresh();
+
+        return $subnet->allMediaCollections()->map(function($collection_name, $key) use ($subnet) {
+            $filename = ($subnet->getFirstMedia($collection_name)) ? $subnet->getFirstMedia($collection_name)->file_name : false;
+
+            return [
+                'collection' => $collection_name,
+                'origin_exists' => $this->isBuilt($subnet, $collection_name),
+                'origin_path' => $this->getOriginPath($subnet, $collection_name),
+                'is_deployed' => $this->isDeployed($subnet, $collection_name),
+                'deploy_path' => ($this->isDeployed($subnet, $collection_name)) ? $this->getDeployPath($subnet, $collection_name) : null,
+            ];
+        });
+    }
+
+    /**
+     * @param  Subnet  $subnet
+     * @param  string  $collection_name MediaLirbary collection name
      * @return boolean
      */
-    public function fileExists(Subnet $subnet)
+    public function isBuilt(Subnet $subnet, $collection_name)
     {
-        if ( ! $subnet->getFirstMedia($this->media_collection) ) {
-            return false;
-        }
-
-        return true;
+        return ($subnet->getFirstMedia($collection_name)) ? true : false;
     }
 
     /**
-     * @param  Subnet $subnet
+     * @param  Subnet  $subnet
+     * @param  string  $collection_name MediaLirbary collection name
      * @return boolean
      */
-    public function option43FileExists(Subnet $subnet)
+    public function isDeployed(Subnet $subnet, $collection_name)
     {
-        if ( ! $subnet->media()->whereFileName($this->getOption43Filename($subnet))->first() ) {
-            return false;
+        return Storage::disk($this->dhcp_configs_disk)->exists($this->getDeployPath($subnet, $collection_name));
+    }
+
+    /**
+     * @param  Subnet  $subnet
+     * @param  string  $collection_name MediaLirbary collection name
+     * @return string
+     */
+    public function getDeployPath(Subnet $subnet, $collection_name)
+    {
+        if ($this->isBuilt($subnet, $collection_name)) {
+            return $this->medialibraray_path_generator->getDeployPath($subnet->getFirstMedia($collection_name));
+        }
+    }
+
+    /**
+     * @param  Subnet  $subnet
+     * @param  string  $collection_name MediaLirbary collection name
+     * @return string
+     */
+    public function getOriginPath(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
+    {
+        if ($this->isBuilt($subnet, $collection_name)) {
+            $path = $this->medialibraray_path_generator->getPath($subnet->getFirstMedia($collection_name));
+            $filename = $subnet->getFirstMedia($collection_name)->file_name;
+            return $path . $filename;
+        }
+    }
+
+    /**
+     * Create the MediaLibrary Media in the origins directory
+     * @param  Subnet $subnet
+     * @param  string $collection_name
+     * @return Spatie\MediaLibrary\Models\Media
+     */
+    public function build(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
+    {
+        return $subnet->addMedia($this->commitFileToTempStorage($subnet, $collection_name))
+            ->toMediaCollection($collection_name, $this->dhcp_origins_disk);
+    }
+
+    /**
+     * Remove the file from the DeployPath
+     * @param  Subnet $subnet
+     * @param  string $collection_name
+     * @return App\Subnet
+     */
+    public function deploy(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
+    {
+        if ($this->isBuilt($subnet, $collection_name)) {
+            $media = $subnet->getFirstMedia($collection_name);
+            $origin_path = $this->medialibraray_path_generator->getPath($media) . $media->file_name;
+            $origin_file = Storage::disk($this->dhcp_origins_disk)->get($origin_path);
+            $deployment = Storage::disk($this->dhcp_configs_disk)->put($this->medialibraray_path_generator->getDeployPath($media), $origin_file);
         }
 
-        return true;
+        return $subnet;
     }
 
     /**
+     * Remove the file from the DeployPath
      * @param  Subnet $subnet
-     * @return void
+     * @param  string $collection_name
+     * @return App\Subnet
      */
-    public function deleteFile(Subnet $subnet)
+    public function undeploy(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
     {
-        $subnet->getFirstMedia($this->media_collection)->delete();
+        if ($this->isDeployed($subnet, $collection_name)) {
+            Storage::disk($this->dhcp_configs_disk)->delete($this->getDeployPath($subnet, $collection_name));
+        }
+
+        return $subnet;
     }
 
     /**
+     * Remove the file from the DeployPath and remove the Media item.
+     * Note: this means we never rid ourselves of the Media item without
+     * first removing the deployed file.
      * @param  Subnet $subnet
-     * @return void
+     * @param  string $collection_name
+     * @return App\Subnet
      */
-    public function deleteOption43File(Subnet $subnet)
+    public function destroy(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
     {
-        $subnet->media()->whereFileName($this->getOption43Filename($subnet))->first()->delete();
+        if ($this->isDeployed($subnet, $collection_name)) {
+            Storage::disk($this->dhcp_configs_disk)->delete($this->getDeployPath($subnet, $collection_name));
+        }
+
+        if ($this->isBuilt($subnet, $collection_name)) {
+            $subnet->clearMediaCollection($collection_name);
+        }
+
+        return $subnet;
     }
 
     /**
      * @param  Subnet $subnet
-     * @return Spatie\MediaLibrary\Models\Media
-     */
-    // public function getDhcpFile(Subnet $subnet)
-    // {
-    //     return $subnet->getFirstMedia($this->media_collection);
-    // }
-
-    /**
-     * @param  Subnet $subnet
-     * @return Spatie\MediaLibrary\Models\Media
-     */
-    public function writeFile(Subnet $subnet)
-    {
-        return $subnet->addMedia($this->commitFileToTempStorage($subnet))
-            ->toMediaCollection($this->media_collection, $this->media_disk);
-    }
-
-    /**
-     * @param  Subnet $subnet
-     * @return Spatie\MediaLibrary\Models\Media
-     */
-    public function writeOption43File(Subnet $subnet)
-    {
-        return $subnet->addMedia($this->commitFileToTempStorage($subnet, 'option43'))
-            ->toMediaCollection($this->media_collection, $this->media_disk);
-    }
-
-    /**
-     * @param  Subnet $subnet
-     * @param  string $type default|option43
+     * @param  string $collection_name
      * @return string|false String will be disk path to file for addMedia() to use
      */
-    public function commitFileToTempStorage(Subnet $subnet, $type = 'default')
+    protected function commitFileToTempStorage(Subnet $subnet, $collection_name = 'dhcp_subnet_definition')
     {
-        if ($type == 'default') {
-            $file_name = $this->getDhcpFilename($subnet);
-            $file_content = $this->generateFileContent($subnet);
-            $path = $this->temp_storage->path($file_name);
+        $dhcp_option_class = '\App\GoldAccess\Dhcp\Options\\' . $subnet->media_collections[$collection_name];
 
-            if ( ! $this->temp_storage->put($file_name, $file_content) ) {
-                return false;
-            }
+        $file_name = $dhcp_option_class::getFilename($subnet);
+        $file_content = $dhcp_option_class::make($subnet);
 
-            return $path;
+        $path = $this->temp_storage->path($file_name);
+
+        if ( ! $this->temp_storage->put($file_name, $file_content) ) {
+            return false;
         }
 
-        if ($type == 'option43') {
-            $file_name = $this->getOption43Filename($subnet);
-            $file_content = $this->generateOption43Content($subnet);
-            $path = $this->temp_storage->path($file_name);
-
-            if ( ! $this->temp_storage->put($file_name, $file_content) ) {
-                return false;
-            }
-
-            return $path;
-        }
-    }
-
-    /**
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function generateFileContent(Subnet $subnet)
-    {
-        $name = $subnet->dhcp_shared_network->name;
-        $vlan = $subnet->dhcp_shared_network->vlan ?: 'none';
-        $dnsmasq_tag = $this->getDnsmasqTag($subnet);
-
-        return '# DHCP Range For ' . $name . ' On VLAN ' . $vlan . "\n" .
-            'dhcp-range=set:"'.$dnsmasq_tag.'",' . $this->rangeFor($subnet) . ',1h' . "\n" .
-            'dhcp-option=tag:"'.$dnsmasq_tag.'",3,'.$subnet->routers;
-    }
-
-    /**
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function generateOption43Content(Subnet $subnet)
-    {
-
-        return Option43::make($this->getDnsmasqTag($subnet), config('goldaccess.settings.acs_url'));
-
-    }
-
-    /**
-     * Dnsmasq range string
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function rangeFor(Subnet $subnet)
-    {
-        return $subnet->start_ip . ',' . $subnet->end_ip . ',' . $subnet->subnet_mask;
-    }
-
-    /**
-     * Generate the filename
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function getDhcpFilename(Subnet $subnet)
-    {
-        return $this->getDnsmasqTag($subnet) . '.conf';
-    }
-
-    /**
-     * Generate the filename
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function getOption43Filename(Subnet $subnet)
-    {
-        return $this->getDnsmasqTag($subnet) . '-option43.conf';
-    }
-
-    /**
-     * Generate the filename
-     * @param  Subnet $subnet
-     * @return string
-     */
-    public function getDnsmasqTag(Subnet $subnet)
-    {
-        return $subnet->dhcp_shared_network->slug . '-' . preg_replace('/\./', '_', $subnet->network_address);
+        return $path;
     }
 }

20a14f2e85af24837b86921eed27b89890a048ef
Goat <ryantgray@gmail.com>
2019-10-17 17:11:49 -0500

    Stop seeding GaSettings

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(GaSettingsSeeder::class);
-
         $this->call(StatesSeeder::class);
 
         $this->call(PlatformsAndModuleTypesSeeder::class);

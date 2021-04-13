408237ea09dc75b8f3f4186ad069ed08464b4c2c
Goat <ryantgray@gmail.com>
2021-04-13 14:42:00 -0500

    Add a "speeds" queue.

--- config/horizon.php
+++ config/horizon.php
@@ -74,7 +74,7 @@ return [
         'production' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue' => ['default', 'onts', 'docker'],
+                'queue' => ['default', 'onts', 'docker', 'speeds'],
                 'balance' => 'simple',
                 'processes' => 10,
                 'tries' => 1,
@@ -84,7 +84,7 @@ return [
         'local' => [
             'supervisor-1' => [
                 'connection' => 'redis',
-                'queue' => ['default', 'onts', 'docker'],
+                'queue' => ['default', 'onts', 'docker', 'speeds'],
                 'balance' => 'simple',
                 'processes' => 3,
                 'tries' => 1,

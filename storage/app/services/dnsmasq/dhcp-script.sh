d14f1ea3dd0afb9b6b24a117d70b0dfd86060e74
Goat <ryantgray@gmail.com>
2021-06-25 14:03:09 -0500

    Update dhcp-script to allow it to just log locally instead of send to GA (optional). Update dhcp-lease-max to 2500.

--- storage/app/services/dnsmasq/dhcp-script.sh
+++ storage/app/services/dnsmasq/dhcp-script.sh
@@ -75,8 +75,10 @@ generate_post_data()
     echo "{\"ACTION\": \"$action\",\"HOSTMAC\":\"$mac\",\"IP\":\"$ip\",\"HOSTNAME\":\"$hostname\",\"DNSMASQ_LEASE_EXPIRES\":\"$DNSMASQ_LEASE_EXPIRES\",\"DNSMASQ_LEASE_LENGTH\":\"$DNSMASQ_LEASE_LENGTH\",\"DNSMASQ_DOMAIN\":\"$DNSMASQ_DOMAIN\",\"DNSMASQ_SUPPLIED_HOSTNAME\":\"$DNSMASQ_SUPPLIED_HOSTNAME\",\"DNSMASQ_TIME_REMAINING\":\"$DNSMASQ_TIME_REMAINING\",\"DNSMASQ_OLD_HOSTNAME\":\"$DNSMASQ_OLD_HOSTNAME\",\"DNSMASQ_INTERFACE\":\"$DNSMASQ_INTERFACE\",\"DNSMASQ_RELAY_ADDRESS\":\"$DNSMASQ_RELAY_ADDRESS\",\"DNSMASQ_TAGS\":\"$DNSMASQ_TAGS\",\"DNSMASQ_LOG_DHCP\":\"$DNSMASQ_LOG_DHCP\",\"DNSMASQ_CLIENT_ID\":\"$DNSMASQ_CLIENT_ID\",\"DNSMASQ_CIRCUIT_ID\":\"$DNSMASQ_CIRCUIT_ID\",\"DNSMASQ_SUBSCRIBER_ID\":\"$DNSMASQ_SUBSCRIBER_ID\",\"DNSMASQ_REMOTE_ID\":\"$DNSMASQ_REMOTE_ID\",\"DNSMASQ_VENDOR_CLASS\":\"$DNSMASQ_VENDOR_CLASS\",\"DNSMASQ_REQUESTED_OPTIONS\":\"$DNSMASQ_REQUESTED_OPTIONS\"}"
 }
 
-curl -i \
--H "Accept: application/json" \
--H "Content-Type:application/json" \
--H "X-CSRF-TOKEN:LGri4hy2pGlx9wVpVvVqTHRwwavZVn2vYu2PS4a2" \
--X POST --data "$(generate_post_data)" "http://$destination/api/dnsmasq/events"
+#curl -i \
+#-H "Accept: application/json" \
+#-H "Content-Type:application/json" \
+#-H "X-CSRF-TOKEN:LGri4hy2pGlx9wVpVvVqTHRwwavZVn2vYu2PS4a2" \
+#-X POST --data "$(generate_post_data)" "http://$destination/api/dnsmasq/events"
+
+#echo "$(generate_post_data)" >> /etc/dnsmasq/dhcp-script.log

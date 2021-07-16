f8b40cefcd622168753557f99d74ad74fe6fdbd5
Goat <ryantgray@gmail.com>
2021-07-16 14:01:35 -0500

    This makes ManagementIp::class get the subscriber_id off of the Port::class instead of building it up from the ProvisioningRecord::class.

--- app/GoldAccess/Dhcp/Options/ManagementIp.php
+++ app/GoldAccess/Dhcp/Options/ManagementIp.php
@@ -72,9 +72,6 @@ class ManagementIp extends DhcpOption
      */
     protected function getSubscriberId(Deployable $provisioning_record)
     {
-        return $provisioning_record->port->slot->aggregator->slug . '/' .
-            $provisioning_record->port->slot->slot_number . '/' .
-            $provisioning_record->port->module . '/' .
-            $provisioning_record->port->port_number;
+        return $provisioning_record->port->subscriber_id;
     }
 }
